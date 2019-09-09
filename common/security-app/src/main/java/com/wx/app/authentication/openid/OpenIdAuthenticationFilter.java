3c16eb695a4f0ccb626a387d8ac851924f150c7d
wangshuai@mzhtechnologies.com <wangshuai@mzhtechnologies.com>
2019-09-09 18:42:05 +0800

    2019/9/9 #1

---
+++ common/security-app/src/main/java/com/wx/app/authentication/openid/OpenIdAuthenticationFilter.java
@@ -0,0 +1,11 @@
+package com.wx.app.authentication.openid;
+
+import com.wx.properties.SecurityConstants;
+
+/**
+ * The class open id authentication filter
+ */
+public class OpenIdAuthenticationFilter {
+    // ~ Static fields/initializers
+    private String openIdParameter = SecurityConstants.DEFAULT_PARAMETER_NAME_OPENID;
+}
---
+++ common/security-core/src/main/java/com/wx/properties/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.wx.properties;
+
+/**
+ * The interface Security constants.
+ */
+public interface SecurityConstants {
+    /**
+     * openid 参数名
+     */
+    String DEFAULT_PARAMETER_NAME_OPENID = "openId";
+}
